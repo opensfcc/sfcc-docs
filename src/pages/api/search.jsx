7c62323c334e2385d8704d8e355716e6df8cb0b7
Peter Schmalfeldt <>
2023-11-10 23:35:14 -0500

    Add support to store search results as JSON with unique cache key for URL param.
    Way faster to store cached results per unique search vs refetching the data each time. Data will only change monthly, at most, so once a unique search is done, there is no need to do it again.
    
    There is a cache clear process added to the NPM scripts so after restarting the server, all saved cached will be removed.  Lots of disc space on the server, so this is a way more performant solution.

--- src/pages/api/search.jsx
+++ src/pages/api/search.jsx
@@ -1,57 +1,79 @@
+import 'dotenv/config'
+import fs from 'fs'
+import md5 from 'md5'
+
 import { getMeta } from '@/data/meta'
 import { currentVersion } from '@/data/versions'
-import 'dotenv/config'
 
 export default function handler(req, res) {
-  // Load Search Function
-  import('@/markdoc/search.mjs').then(({ search }) => {
+  // Setup Cache Key
+  const hashKey = md5(`hash-${req.query.query}-${req.query.limit || 100}-${req.query.offset || 0}`)
+  const cacheFile = process.cwd() + `/.cache/${hashKey}.json`
+
+  if (fs.existsSync(cacheFile)) {
     try {
-      // Perform Search
-      const baseURL = process.env.NEXT_PUBLIC_SITE_URL || 'https://sfccdocs.com'
-      const results = search(req.query.query, {
-        limit: req.query.limit ? parseInt(req.query.limit, 10) : 100,
-        offset: req.query.offset ? parseInt(req.query.offset, 10) : 0,
-      })
-
-      // Loop through results
-      results.map((result, index) => {
-        // Add additional properties
-        const parts = result.url ? result.url.split('#') : null
-        const baseUri = parts[0]
-        const anchor = parts.length > 1 ? `#${parts[1]}` : ''
-        const deprecated = baseUri.startsWith('/deprecated/')
-        const meta = getMeta(baseUri, deprecated)
-
-        result.deprecated = deprecated
-        result.title = meta.title
-        result.description = meta.description
-        result.keywords = meta.nav?.alt ? meta.nav.alt.split(' › ') : null
-        result.url = `${baseURL}${result.url}`
-        result.embed = `${baseURL}${baseUri}?embed=true${anchor}`
-        result.content = result.content ? result.content.split('. ')[0] : null
-
-        // Remove unneeded properties
-        delete result.score
-
-        if (result.pageTitle) {
-          delete result.pageTitle
-        }
-
-        // Sort properties alphabetically for easier readability
-        results[index] = Object.keys(result)
-          .sort()
-          .reduce((accumulator, key) => {
-            accumulator[key] = result[key]
-
-            return accumulator
-          }, {})
-      })
-
-      // Return results
-      res.status(200).json({ total: results.length, results, version: currentVersion })
+      const cachedJson = fs.readFileSync(cacheFile, 'utf8')
+      res.setHeader('Cache-Control', 's-maxage=86400')
+      res.status(200).json(JSON.parse(cachedJson))
     } catch (error) {
       // Return error
       res.status(400).json({ error: error.message, trace: error.stack })
     }
-  })
+  } else {
+    // Load Search Function
+    import('@/markdoc/search.mjs').then(({ search }) => {
+      try {
+        // Perform Search
+        const baseURL = process.env.NEXT_PUBLIC_SITE_URL || 'https://sfccdocs.com'
+        const results = search(req.query.query, {
+          limit: req.query.limit ? parseInt(req.query.limit, 10) : 100,
+          offset: req.query.offset ? parseInt(req.query.offset, 10) : 0,
+        })
+
+        // Loop through results
+        results.map((result, index) => {
+          // Add additional properties
+          const parts = result.url ? result.url.split('#') : null
+          const baseUri = parts[0]
+          const anchor = parts.length > 1 ? `#${parts[1]}` : ''
+          const deprecated = baseUri.startsWith('/deprecated/')
+          const meta = getMeta(baseUri, deprecated)
+
+          result.deprecated = deprecated
+          result.title = meta.title
+          result.description = meta.description
+          result.keywords = meta.nav?.alt ? meta.nav.alt.split(' › ') : null
+          result.url = `${baseURL}${result.url}`
+          result.embed = `${baseURL}${baseUri}?embed=true${anchor}`
+          result.content = result.content ? result.content.split('. ')[0] : null
+
+          // Remove unneeded properties
+          delete result.score
+
+          if (result.pageTitle) {
+            delete result.pageTitle
+          }
+
+          // Sort properties alphabetically for easier readability
+          results[index] = Object.keys(result)
+            .sort()
+            .reduce((accumulator, key) => {
+              accumulator[key] = result[key]
+
+              return accumulator
+            }, {})
+        })
+
+        const data = { total: results.length, results, version: currentVersion }
+        fs.writeFileSync(cacheFile, JSON.stringify(data))
+
+        // Return results
+        res.setHeader('Cache-Control', 's-maxage=86400')
+        res.status(200).json(data)
+      } catch (error) {
+        // Return error
+        res.status(400).json({ error: error.message, trace: error.stack })
+      }
+    })
+  }
 }
